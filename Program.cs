edc0ffae877e1930769dedde630530b4287cd934
nayasuna <nayasuna01@students.amikom.ac.id>
2022-07-16 10:28:54 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    class Program
+    {
+        // deklarasi objek collection untuk menampung objek mahasiswa
+        static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>();
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Responsi UAS Matakuliah Pemrograman";
+
+            while (true)
+            {
+                TampilMenu();
+
+                Console.Write("\nNomor Menu [1..3]: ");
+                int nomorMenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (nomorMenu)
+                {
+                    case 1:
+                        TambahMahasiswa();
+                        break;
+
+                    case 2:
+                        TampilMahasiswa();
+                        break;
+
+                    case 3: // keluar dari program
+                        return;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void TampilMenu()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menampilkan menu
+            Console.WriteLine("Pilih Menu Aplikasi");
+            Console.WriteLine("1. Tambah Mahasiswa");
+            Console.WriteLine("2. Tampilkan Mahasiswa");
+            Console.WriteLine("3. Keluar");
+        }
+
+        static void TambahMahasiswa()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menambahkan objek mahasiswa ke dalam collection
+            Mahasiswa mhs = new Mahasiswa();
+            Console.WriteLine("Tambah Data Mahasiswa");
+            Console.WriteLine();
+            Console.Write("Nim :  ");
+            mhs.Nim = Console.ReadLine();
+            Console.Write("Nama :  ");
+            mhs.Nama = Console.ReadLine();
+            Console.Write("Jenis Kelamin [L/P] :  ");
+            mhs.JenisKelamin = Console.ReadLine().ToUpper() == "L" ? "laki-laki" : "Perempuan";
+            Console.Write("IPK :  ");
+            mhs.IPK = Console.ReadLine();
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+            daftarMahasiswa.Add(mhs);
+        }
+
+        static void TampilMahasiswa()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menampilkan daftar mahasiswa yang ada di dalam collection
+            Console.WriteLine("Data Mahasiswa");
+            Console.WriteLine();
+            var nomer = 1;
+
+            foreach (Mahasiswa mhs in daftarMahasiswa)
+            {
+                Console.WriteLine("{0}. {1}, {2}, {3}, {4}", nomer, mhs.Nim, mhs.Nama, mhs.JenisKelamin, mhs.IPK);
+                nomer++;
+            }
+
+            Console.WriteLine("\nTekan enter untuk kembali ke menu");
+            Console.ReadKey();
+        }
+    }
+}
+
